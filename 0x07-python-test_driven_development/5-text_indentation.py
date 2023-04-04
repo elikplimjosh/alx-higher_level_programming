a08df1f4341c26c078982ef58eb62ea260b5d5ca
elikplimjosh <jayakakpo4u2@gmail.com>
2023-04-04 15:48:56 -0700

    update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Function to break a text with two new lines at certain characters
+
+"""
+def text_indentation(text):
+    """
+    checks that text arg is valid string
+    then implement logic to perform spiting
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    i = 0
+
+    for a in text:
+        if i == 0:
+            if a == ' ':
+                continue
+            else:
+                i = 1
+        if i == 1:
+            if a == '?' or a == '.' or a == ':':
+                print(a)
+                print()
+                i = 0
+            else:
+                print(a, end="")

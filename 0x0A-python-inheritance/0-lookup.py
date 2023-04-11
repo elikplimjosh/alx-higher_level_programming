00270408abd885af9cf2920eb89a4531d750a3e3
elikplimjosh <jayakakpo4u2@gmail.com>
2023-04-11 15:01:03 -0700

    task 0 && Readme

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates a function for task 0"""
+
+
+def lookup(obj):
+    """
+       A function that retrieves a list of the attributes and
+       methods that are currently accessible to an object
+    """
+    return dir(obj)

e93b3a7b77e886932dee8f7561784bcaec3fc4c0
elikplimjosh <jayakakpo4u2@gmail.com>
2023-04-12 09:26:10 -0700

    task 3 & 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    module to convert JSON string to python object
+"""
+
+import json
+
+def from_json_string(my_str):
+    """
+        function converts JSON to python object
+    """
+    return(json.loads(my_str))i
